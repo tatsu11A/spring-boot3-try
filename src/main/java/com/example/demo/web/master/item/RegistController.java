0a3effcd6377118871c88940d012e6012ca84cfa
Tatsuya Asagoshi <asakoshi@insightech.co.jp>
2026-03-12 16:58:55 +0900

    コミット

--- src/main/java/com/example/demo/web/master/item/RegistController.java
+++ src/main/java/com/example/demo/web/master/item/RegistController.java
@@ -1,39 +1,57 @@
 package com.example.demo.web.master.item;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Item;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequestMapping("/master/item")
 @RequiredArgsConstructor
 public class RegistController {
 
     private final ItemRegistService itemRegistService;
 
-    @GetMapping("/master/item/index")
+    @GetMapping("/index")
     public String index(@ModelAttribute("form") ItemForm form) {
         return "master/item/index";
     }
 
-    @PostMapping("/master/item/confirm")
+    @PostMapping("/confirm")
     public String confirm(@ModelAttribute("form") ItemForm form) {
         return "master/item/confirm";
     }
 
-    @PostMapping("/master/item/regist")
+    @PostMapping("/regist")
     public String regist(@ModelAttribute("form") ItemForm form) {
         Item item = form.toItem();
         itemRegistService.regist(item);
         return "redirect:/master/item/complete";
     }
 
-    @GetMapping("/master/item/complete")
+    @GetMapping("/complete")
     public String complete() {
         return "master/item/complete";
+    }
+}
+
+@Data
+class ItemForm {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+
+    public Item toItem() {
+        Item item = new Item();
+        BeanUtils.copyProperties(this, item);
+        return item;
     }
 }
